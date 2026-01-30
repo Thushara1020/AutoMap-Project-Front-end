082d023e3f9834576380e9d8031940e656aec6fd
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-30 16:12:20 +0530

    remove cars component

--- src/app/page/cars/cars.component.ts
+++ src/app/page/cars/cars.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 import { FooterComponent } from "../../component/footer/footer.component";
+import { CarlistComponent } from "../../component/carlist/carlist.component";
 
 @Component({
   selector: 'app-cars',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CarlistComponent],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
