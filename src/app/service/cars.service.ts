373a2ee35350c6ee03e450f1abedc04a5d2b4dd0
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-01-30 16:14:08 +0530

    add CarsService to fetch data from API

---
+++ src/app/service/cars.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarsService {
+
+  constructor(private http: HttpClient) { }
+
+  getCars() {
+    return this.http.get('http://localhost:8080/api/cars/all');
+  }
+}
