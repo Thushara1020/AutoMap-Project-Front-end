b6427b2085198cde5f25115c8f1119021d9cccc0
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-02 19:44:03 +0530

    add getCars method to CarService

---
+++ src/app/service/carservice/car.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+  private apiUrl = 'http://localhost:8080/api/cars/all';
+
+  constructor(private http: HttpClient) { }
+
+  getCars(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
