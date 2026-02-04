d06ece9a7031b896ea6ce23ccd019b68e7cb74c3
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-04 23:48:54 +0530

    add brands route and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
 import { CarsComponent } from './page/cars/cars.component';
+import { BrandsComponent } from './page/brands/brands.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'cars',
         component: CarsComponent
+    },
+    {
+        path:'brand',
+        component: BrandsComponent
     }
 ];
